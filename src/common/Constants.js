186ce1734dbc34628125dacf0dd909cf686967be
snelpatel <snelpatel@gmail.com>
2021-05-14 11:43:28 +0530

    API and other constants added

---
+++ src/common/Constants.js
@@ -0,0 +1,20 @@
+export const ApiRequestTypeEnum = Object.freeze({
+  GET: "GET",
+  POST: "POST",
+  PUT: "PUT",
+  DELETE: "DELETE"
+});
+  
+export const ValueTypeEnum = Object.freeze({
+  FORM_FIELD: 1,
+  VALIDATION_MESSAGE: 2
+});
+  
+export const DisplayClassname = Object.freeze({
+  DISPLAY_NONE: "dispNone",
+  DISPLAY_BLOCK: "dispBlock"
+});
+
+  
+export const LOGIN_CREDENTIALS_ERROR_MSG = "Invalid credentials!";
+  
